9f773e723352afa41227127a4b1c56d1481f8f7a
just-developer-ux <62999702+just-developer-ux@users.noreply.github.com>
2020-10-19 21:34:19 +0300

    Очистка
    Оптимізація розробки погодника

--- JS/scroll.js
+++
@@ -1,6 +0,0 @@
-// let slides = document.querySelector('.slides'),
-//    windowScroll = document.slides.scrollLeft,
-//    sliderWidth = document.documentElement.scrollWidth - document.documentElement.clientWidth,
-//    scrolled = (windowScroll / sliderWidth) * 100;
-
-// document.querySelector
